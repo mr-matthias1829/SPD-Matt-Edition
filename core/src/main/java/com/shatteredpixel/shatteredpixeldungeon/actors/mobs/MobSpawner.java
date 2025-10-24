db170204d736b3f209848c268e84d05676deb85c
mr-matthias1829 <180924703+mr-matthias1829@users.noreply.github.com>
2025-10-24 14:30:06 +0200

    v1.132

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
@@ -75,8 +75,7 @@ public class MobSpawner extends Actor {
 				//3x rat, 1x snake
 				return new ArrayList<>(Arrays.asList(
 						Rat.class, Rat.class, Rat.class,
-						Snake.class,
-                        DM151.class));
+						Snake.class));
 			case 2:
 				//2x rat, 1x snake, 2x gnoll
 				return new ArrayList<>(Arrays.asList(Rat.class, Rat.class,
