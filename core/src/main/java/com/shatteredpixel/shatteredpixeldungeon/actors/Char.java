353c62446ab7d4368ea4731469d681843de448df
Evan Debenham <evan@shatteredpixel.com>
2026-03-25 23:19:49 +0100

    v4.0.0: fixed some fx that handled brute rage not handling armored rage
    (cherry picked from commit 56de7e8ac0c27df4b3447bdedae348e1af598f41)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -501,8 +501,8 @@ public abstract class Char extends Actor {
 
 			if (enemy.isAlive() && enemy.alignment != alignment && prep != null && prep.canKO(enemy)){
 				enemy.HP = 0;
-				if (enemy.buff(Brute.BruteRage.class) != null){
-					enemy.buff(Brute.BruteRage.class).decShield(enemy.buff(Brute.BruteRage.class).shielding());
+				for (Buff b : enemy.buffs(Brute.BruteRage.class)){
+					b.detach();
 				}
 				if (!enemy.isAlive()) {
 					enemy.die(this);
@@ -522,8 +522,8 @@ public abstract class Char extends Actor {
 						&& !Char.hasProp(enemy, Property.MINIBOSS) &&
 						(enemy.HP/(float)enemy.HT) <= 0.4f*((Hero)this).pointsInTalent(Talent.COMBINED_LETHALITY)/3f) {
 					enemy.HP = 0;
-					if (enemy.buff(Brute.BruteRage.class) != null){
-						enemy.buff(Brute.BruteRage.class).decShield(enemy.buff(Brute.BruteRage.class).shielding());
+					for (Buff b : enemy.buffs(Brute.BruteRage.class)){
+						b.detach();
 					}
 					if (!enemy.isAlive()) {
 						enemy.die(this);
