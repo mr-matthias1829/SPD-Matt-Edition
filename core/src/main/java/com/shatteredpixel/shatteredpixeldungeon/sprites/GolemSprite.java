e0edbc98cfe9b5b0c0319738664d95255861df8a
Evan Debenham <evan@shatteredpixel.com>
2026-04-02 23:41:09 +0200

    v4.0.0: moved all vault mobs to new class structure, much cleaner!

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/GolemSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/GolemSprite.java
@@ -121,8 +121,6 @@ public class GolemSprite extends MobSprite {
 					public void call() {
 						if (ch instanceof Golem) {
 							((Golem) ch).onZapComplete();
-						} else if (ch instanceof VaultGolem){
-							((VaultGolem) ch).onZapComplete();
 						}
 					}
 				}, true );
