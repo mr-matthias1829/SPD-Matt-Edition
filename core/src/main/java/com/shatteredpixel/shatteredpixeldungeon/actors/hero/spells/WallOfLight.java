55f70a4cf824b6c9ec09ffdaecf7fb3b091630cf
Evan Debenham <Evan@ShatteredPixel.com>
2026-05-01 12:29:45 -0400

    v4.0.0: consistency pass on blobs visible outside of FOV
    (cherry picked from commit 8709fc42b94b3599dc2318d9d71e479ff2f1304c)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/spells/WallOfLight.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/spells/WallOfLight.java
@@ -247,6 +247,10 @@ public class WallOfLight extends TargetedClericSpell {
 
 	public static class LightWall extends Blob {
 
+		{
+			alwaysVisible = true;
+		}
+
 		@Override
 		protected void evolve() {
 
