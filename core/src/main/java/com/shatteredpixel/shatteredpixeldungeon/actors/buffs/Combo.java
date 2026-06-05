8c8f885a4db8218f9221e862765643d654ad42c2
Evan Debenham <Evan@ShatteredPixel.com>
2026-06-05 14:46:12 -0400

    v4.0.0: fixed a visual quirk when fury is interrupted by scene reset
    (cherry picked from commit df03ba4378dbfada837f6eb9ef3d844f20fe1697)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Combo.java
@@ -445,6 +445,7 @@ public class Combo extends Buff implements ActionIndicator.Action {
 				if (count > 0){
 					furyHitsLeft = count;
 					count = 0;
+					detach();
 					hero.spend(hero.attackDelay());
 				}
 				furyHitsLeft--;
@@ -459,7 +460,6 @@ public class Combo extends Buff implements ActionIndicator.Action {
 					});
 				} else {
 					furyHitsLeft = 0;
-					detach();
 					Sample.INSTANCE.play(Assets.Sounds.HIT_STRONG);
 					ActionIndicator.clearAction(Combo.this);
 					hero.next();
