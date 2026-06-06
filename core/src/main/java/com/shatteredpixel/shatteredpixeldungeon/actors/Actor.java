08ac3fe004d1c2d478ed224be410e1dfb8224419
Evan Debenham <evan@shatteredpixel.com>
2026-06-06 20:49:41 +0200

    v4.0.0: improved internal swarm intel logic
    (cherry picked from commit 06d1c8ff75978b0932aadb0875114b3e8cc6b930)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Actor.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Actor.java
@@ -106,6 +106,11 @@ public abstract class Actor implements Bundlable {
 	public void timeToNow() {
 		time = now;
 	}
+
+	//used when now is being cleared as a part of statix fixTime()
+	public void fixTime(float decrement){
+		time -= decrement;
+	}
 	
 	protected void diactivate() {
 		time = Float.MAX_VALUE;
@@ -185,7 +190,7 @@ public abstract class Actor implements Bundlable {
 		//So that turns always align with a whole number
 		min = (int)min;
 		for (Actor a : all) {
-			a.time -= min;
+			a.fixTime(min);
 		}
 
 		if (Dungeon.hero != null && all.contains( Dungeon.hero )) {
