5ede18ef5f223613df290e4ef7295a2ae1dfd35a
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-07 13:30:52 -0400

    v4.0.0: vault rings can now ID with kills & final rings ID on boss death
    (cherry picked from commit f6f02897e70da30218296bbb03c0f7b4ad72ab7f)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Imp.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/Imp.java
@@ -318,7 +318,7 @@ public class Imp extends NPC {
 					((Artifact)artif.identify(false)).transferUpgrade(5);
 				} else {
 					artif = Generator.random(Generator.Category.RING);
-					artif.levelKnown = artif.cursedKnown = true;
+					//we delay the ID on rings until the boss is defeated
 					artif.upgrade(Random.IntRange(2, 4));
 				}
 				rewardOptions.add(artif);
@@ -327,7 +327,7 @@ public class Imp extends NPC {
 				do {
 					ring = Generator.random(Generator.Category.RING);
 				} while (ring.getClass() == artif.getClass()); //rare cases of the same kind of ring twice
-				ring.levelKnown = ring.cursedKnown = true;
+				//we delay the ID on rings until the boss is defeated
 				ring.upgrade(Random.IntRange(2, 4));
 				rewardOptions.add(ring);
 
