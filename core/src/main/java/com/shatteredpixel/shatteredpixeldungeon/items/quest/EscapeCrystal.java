226dbc96705ec0b4e98dbe11f2dcd55f102b7692
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-10 13:37:39 -0400

    v4.0.0: entering/exiting the vault now fully heals the hero
    (cherry picked from commit 6550528f65aa7dee87b2a9e4d506fbe6cee2b191)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
@@ -256,6 +256,7 @@ public class EscapeCrystal extends Item {
 				b.detach();
 			}
 		}
+		Dungeon.hero.HP = Dungeon.hero.HT; //full heal
 
 		//logic for removing Warrior's Seal or Mage's staff
 		if (preserve instanceof Armor && ((Armor) preserve).checkSeal() != null){
