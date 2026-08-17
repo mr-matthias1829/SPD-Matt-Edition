4803416de0a221f666944fe0efe808fc01a5c07c
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-17 13:01:13 -0400

    v4.0.0: fixed handling of old saves in the vault tester level
    (cherry picked from commit 2d1fb467effa268d8444fea0b7cb2d58c7965cae)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/quest/EscapeCrystal.java
@@ -88,6 +88,12 @@ public class EscapeCrystal extends Item {
 
 			if (Dungeon.level instanceof VaultLevel){
 
+				//pre-v4.0.0 saves still in the vault tester area
+				if (Imp.Quest.isOld()){
+					leaveVault(null, 0);
+					return;
+				}
+
 				int score = 0;
 
 				//firstly, score is always a full 4k if the hero had the statue
@@ -274,7 +280,7 @@ public class EscapeCrystal extends Item {
 		restoreHeroBelongings(Dungeon.hero, preserve);
 		Dungeon.hero.updateHT(false);
 		detachAll(Dungeon.hero.belongings.backpack);
-		Imp.Quest.complete(score);
+		if (!Imp.Quest.isOld()) Imp.Quest.complete(score);
 
 		Level.beforeTransition();
 		InterlevelScene.curTransition = new LevelTransition(Dungeon.level,
