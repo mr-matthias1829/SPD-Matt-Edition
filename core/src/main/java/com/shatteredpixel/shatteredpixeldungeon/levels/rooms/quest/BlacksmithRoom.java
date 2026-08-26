35c6e892bc07093c84c8385c658f8636c4f18a6b
Evan Debenham <Evan@ShatteredPixel.com>
2026-08-26 12:10:42 -0400

    v4.0.0: fixed blacksmith room limit on top connections not working
    (cherry picked from commit a4b1e26c1ba8d010403a64dbdafe6b247d6506ca)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/quest/BlacksmithRoom.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/rooms/quest/BlacksmithRoom.java
@@ -140,7 +140,7 @@ public class BlacksmithRoom extends StandardRoom {
 
 	@Override
 	public int maxConnections(int direction) {
-		if (direction == top){
+		if (direction == TOP){
 			return 1;
 		} else {
 			return super.maxConnections(direction);
