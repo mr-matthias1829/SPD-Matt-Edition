14631606ca0130276d427161d0866ec476b4ddba
Evan Debenham <evan@shatteredpixel.com>
2026-08-31 22:05:49 +0200

    v4.0.0: made use of custom terrain in various places where appropriate
    (cherry picked from commit 3efd8228a6256c0a2065f6ffe04008badae1c911)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndInfoCell.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/windows/WndInfoCell.java
@@ -62,6 +62,16 @@ public class WndInfoCell extends Window {
 				}
 			}
 		}
+		if (customImage == null){
+			for (CustomTilemap i : Dungeon.level.customTerrain){
+				if ((x >= i.tileX && x < i.tileX+i.tileW) &&
+						(y >= i.tileY && y < i.tileY+i.tileH)){
+					if (i.image(x - i.tileX, y - i.tileY) != null) {
+						customImage = i.image(x - i.tileX, y - i.tileY);
+					}
+				}
+			}
+		}
 
 		if (customImage != null){
 			return customImage;
@@ -90,6 +100,16 @@ public class WndInfoCell extends Window {
 				}
 			}
 		}
+		if (customTile == null){
+			for (CustomTilemap i : Dungeon.level.customTerrain){
+				if ((x >= i.tileX && x < i.tileX+i.tileW) &&
+						(y >= i.tileY && y < i.tileY+i.tileH)){
+					if (i.image(x - i.tileX, y - i.tileY) != null) {
+						customTile = i;
+					}
+				}
+			}
+		}
 		if (customTile != null){
 			x -= customTile.tileX;
 			y -= customTile.tileY;
@@ -114,6 +134,16 @@ public class WndInfoCell extends Window {
 					(y >= i.tileY && y < i.tileY+i.tileH)){
 				if (i.image(x - i.tileX, y - i.tileY) != null) {
 					customTile = i;
+				}
+			}
+		}
+		if (customTile == null){
+			for (CustomTilemap i : Dungeon.level.customTerrain){
+				if ((x >= i.tileX && x < i.tileX+i.tileW) &&
+						(y >= i.tileY && y < i.tileY+i.tileH)){
+					if (i.image(x - i.tileX, y - i.tileY) != null) {
+						customTile = i;
+					}
 				}
 			}
 		}
